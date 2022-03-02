7a36482e06230d533169a0dfa6e45c6ea1f67d49
Matias Ringeling Selman <39034958+mringeling17@users.noreply.github.com>
2022-03-02 13:02:18 -0300

    archivos

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Event\Event;
+use Cake\Http\Exception\NotFoundException;
+
+class UsersController extends AppController
+{
+
+    public function beforeFilter(Event $event){
+        parent::beforeFilter($event);
+        $this->Auth->allow(['add', 'logout']);
+    }
+    public function index(){
+        return $this->redirect(['action' => '/..']);
+    }
+
+     public function list()
+     {
+        $this->set('users', $this->Users->find('all'));
+    }
+
+    public function view($id)
+    {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid user'));
+        }
+
+        $user = $this->Users->get($id);
+        $this->set(compact('user'));
+    }
+
+    public function add()
+    {
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(['action' => 'add']);
+            }
+            $this->Flash->error(__('Unable to add the user.'));
+        }
+        $this->set('user', $user);
+    }
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error(__('Invalid username or password, try again'));
+        }
+    }
+    public function logout(){
+        return $this->redirect($this->Auth->logout());
+    }
+
+    public function delete($id){
+        $this->request->allowMethod(['post', 'delete']);
+
+        $article = $this->Users->get($id);
+        if ($this->Users->delete($article)) {
+            $this->Flash->success(__('El Usuario con id: {0} ha sido eliminado.', h($id)));
+            return $this->redirect(['action' => 'index']);
+        }
+    }
+
+
+}
